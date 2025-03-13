43de04c302205819c40f3355b40dc7c65f32605d
AMIRIOX <wumingyun2120@outlook.com>
2025-03-13 11:58:06 +0800

    [fix] 修复了 Hypr/Waybar 左脑攻击右脑的问题

--- waybar2/scripts/spotify.sh
+++ waybar2/scripts/spotify.sh
@@ -6,17 +6,17 @@ while true; do
 
 	if [ -z "$(playerctl metadata album)" ]; then
 		if [ "$player_status" = "Playing" ]; then
-			echo "$(playerctl metadata artist) - $(playerctl metadata title)"
+			echo "$(playerctl metadata title)"
 		elif [ "$player_status" = "Paused" ]; then
-			echo " $(playerctl metadata artist) - $(playerctl metadata title)"
+			echo " $(playerctl metadata title)"
     else
 			echo ""
 		fi
 	else
 		if [ "$player_status" = "Playing" ]; then
-			echo "<span color='#1db954'></span> $(playerctl metadata artist) - $(playerctl metadata title)"
+			echo "<span color='#1db954'></span> $(playerctl metadata title)"
 		elif [ "$player_status" = "Paused" ]; then
-			echo "<span color='#1db954'></span>  $(playerctl metadata artist) - $(playerctl metadata title)"
+			echo "<span color='#1db954'></span>  $(playerctl metadata title)"
     else
 			echo ""
 		fi
